266d8a65401c3c26d576f8928a8a27a34a896da3
Antônio Alexandre Sobral de Oliveira Cavalcante <127903871+Toin19@users.noreply.github.com>
2024-04-08 23:09:03 -0300

    singleton

---
+++ Singleton/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+        Singleton s4 = Singleton.getInstance();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
